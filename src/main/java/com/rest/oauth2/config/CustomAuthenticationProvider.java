2f4a0a5e0368d8780307ec64f8daf2eb43fd044a
michael2k <kms@noaats.com>
2020-02-29 21:15:55 +0900

    resource server

--- src/main/java/com/rest/oauth2/config/CustomAuthenticationProvider.java
+++ src/main/java/com/rest/oauth2/config/CustomAuthenticationProvider.java
@@ -15,26 +15,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
-    private final PasswordEncoder passwordEncoder;
-    private final UserJpaRepo userJpaRepo;
+	private final PasswordEncoder passwordEncoder;
+	private final UserJpaRepo userJpaRepo;
 
-    @Override
-    public Authentication authenticate(Authentication authentication) {
+	@Override
+	public Authentication authenticate(Authentication authentication) {
 
-        String name = authentication.getName();
-        String password = authentication.getCredentials().toString();
+		String name = authentication.getName();
+		String password = authentication.getCredentials().toString();
 
-        User user = userJpaRepo.findByUid(name).orElseThrow(() -> new UsernameNotFoundException("user is not exists"));
+		User user = userJpaRepo.findByUid(name).orElseThrow(() -> new UsernameNotFoundException("user is not exists"));
 
-        if (!passwordEncoder.matches(password, user.getPassword()))
-            throw new BadCredentialsException("password is not valid");
+		if (!passwordEncoder.matches(password, user.getPassword()))
+			throw new BadCredentialsException("password is not valid");
 
-        return new UsernamePasswordAuthenticationToken(name, password, user.getAuthorities());
-    }
+		return new UsernamePasswordAuthenticationToken(name, password, user.getAuthorities());
+	}
 
-    @Override
-    public boolean supports(Class<?> authentication) {
-        return authentication.equals(
-                UsernamePasswordAuthenticationToken.class);
-    }
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
 }
--- src/main/java/com/rest/oauth2/config/Oauth2AuthorizationConfig.java
+++ src/main/java/com/rest/oauth2/config/Oauth2AuthorizationConfig.java
@@ -70,31 +70,31 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
     /**
      * 토큰 정보를 DB를 통해 관리한다.
      */
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-    	endpoints.tokenStore(new JdbcTokenStore(dataSource)).userDetailsService(userDetailService);
-        //endpoints.tokenStore(new JdbcTokenStore(dataSource));
-    }
+//    @Override
+//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+//    	endpoints.tokenStore(new JdbcTokenStore(dataSource)).userDetailsService(userDetailService);
+//        //endpoints.tokenStore(new JdbcTokenStore(dataSource));
+//    }
 
     /**
      * 토큰 발급 방식을 JWT 토큰 방식으로 변경한다. 이렇게 하면 토큰 저장하는 DB Table은 필요가 없다.
      */
-//    @Override
-//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-//        super.configure(endpoints);
-//        endpoints.accessTokenConverter(jwtAccessTokenConverter()).userDetailsService(userDetailService);
-//        // endpoints.accessTokenConverter(jwtAccessTokenConverter());
-//    }
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        super.configure(endpoints);
+        endpoints.accessTokenConverter(jwtAccessTokenConverter()).userDetailsService(userDetailService);
+        // endpoints.accessTokenConverter(jwtAccessTokenConverter());
+    }
 
     /**
      * jwt converter - signKey 공유 방식
      */
-//    @Bean
-//    public JwtAccessTokenConverter jwtAccessTokenConverter() {
-//        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-//        converter.setSigningKey(signKey);
-//        return converter;
-//    }
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(signKey);
+        return converter;
+    }
 
     /**
      * jwt converter - 비대칭 키 sign
