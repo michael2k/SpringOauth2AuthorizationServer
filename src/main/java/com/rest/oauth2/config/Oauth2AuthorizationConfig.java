dc71c4e730787f45c586e097e990875287b50290
michael2k <kms@noaats.com>
2020-02-29 21:54:41 +0900

    1차 테스

--- src/main/java/com/rest/oauth2/config/Oauth2AuthorizationConfig.java
+++ src/main/java/com/rest/oauth2/config/Oauth2AuthorizationConfig.java
@@ -1,16 +1,12 @@
 package com.rest.oauth2.config;
 
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
-import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
 
 /**
  * id/password 기반 Oauth2 인증을 담당하는 서버
@@ -22,54 +18,17 @@ import javax.sql.DataSource;
 @Configuration
 @EnableAuthorizationServer
 public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
-
-    private final PasswordEncoder passwordEncoder;
-    private final DataSource dataSource;
-
-    /**
-     * 클라이언트 정보 주입 방식을 jdbcdetail로 변경
-     *
-     * @param clients
-     * @throws Exception
-     */
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
-    }
-
-    /**
-     * 토큰 정보를 DB를 통해 관리한다.
-     *
-     * @return
-     */
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.tokenStore(new JdbcTokenStore(dataSource));
-    }
-
-
-//
-//    /**
-//     * 토큰 발급 방식을 JWT 토큰 방식으로 변경한다. 이렇게 하면 토큰 저장하는 DB Table은 필요가 없다.
-//     *
-//     * @param endpoints
-//     * @throws Exception
-//     */
-//    @Override
-//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-//        super.configure(endpoints);
-//        endpoints.accessTokenConverter(jwtAccessTokenConverter());
-//    }
-//
-
-//
-//    /**
-//     * jwt converter를 등록
-//     *
-//     * @return
-//     */
-//    @Bean
-//    public JwtAccessTokenConverter jwtAccessTokenConverter() {
-//        return new JwtAccessTokenConverter();
-//    }
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+			.withClient("testClientId")
+			.secret("testSecret")
+			.redirectUris("http://localhost:8081/oauth2/callback")
+			.authorizedGrantTypes("authorization_code")
+			.scopes("read", "write")
+			.accessTokenValiditySeconds(30000);
+	}
+	
+	
 }
--- src/main/java/com/rest/oauth2/config/SecurityConfig.java
+++ src/main/java/com/rest/oauth2/config/SecurityConfig.java
@@ -1,20 +1,25 @@
 package com.rest.oauth2.config;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final CustomAuthenticationProvider authenticationProvider;
-
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authenticationProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	auth.inMemoryAuthentication()
+    		.withUser("user")
+    		.password("pass")
+    		.roles("USER");
     }
 
     @Override
@@ -27,5 +32,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin().and()
                 .httpBasic();
+    }
+    
+    @Bean
+    public PasswordEncoder noOpPasswordEncoder() {
+    	return NoOpPasswordEncoder.getInstance();
     }
 }
