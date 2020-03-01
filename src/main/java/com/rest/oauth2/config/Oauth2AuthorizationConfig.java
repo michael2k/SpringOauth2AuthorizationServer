17b73623c84f630e174e84615394f57e9830183e
michael2k <kms@noaats.com>
2020-03-01 21:26:45 +0900

    디비로 토큰 관리

--- src/main/java/com/rest/oauth2/config/Oauth2AuthorizationConfig.java
+++ src/main/java/com/rest/oauth2/config/Oauth2AuthorizationConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import lombok.RequiredArgsConstructor;
 
@@ -45,6 +47,16 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
+	}
+	
+	/**
+	 * 토큰 정보를 DB를 통해 관리한다.
+	 * @return
+	 */
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.tokenStore(new JdbcTokenStore(dataSource));
 	}
 	
 	
