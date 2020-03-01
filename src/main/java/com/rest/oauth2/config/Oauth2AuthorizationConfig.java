4aae7081cbeb6b8854cc3d4b7a76c119f2bf7e06
michael2k <kms@noaats.com>
2020-03-01 21:46:05 +0900

    토큰 타입을 JWT 로 변경.

--- src/main/java/com/rest/oauth2/config/Oauth2AuthorizationConfig.java
+++ src/main/java/com/rest/oauth2/config/Oauth2AuthorizationConfig.java
@@ -1,16 +1,16 @@
 package com.rest.oauth2.config;
 
-
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -53,11 +53,29 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
 	 * 토큰 정보를 DB를 통해 관리한다.
 	 * @return
 	 */
-
+//	@Override
+//	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//		endpoints.tokenStore(new JdbcTokenStore(dataSource));
+//	}
+	
+	/**
+	 * 토큰 발급 방식을 JWT 토큰 방식으로 변경한다. 이렇게 하면 토큰 저정하는 DB Table은 필요가 없다.
+	 * @return
+	 */
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.tokenStore(new JdbcTokenStore(dataSource));
+		super.configure(endpoints);
+		endpoints.accessTokenConverter(jwtAccessTokenConverter());
 	}
 	
+	/**
+	 * jwt converter를 등록.
+	 * 
+	 * @return
+	 */
+	@Bean
+	public JwtAccessTokenConverter jwtAccessTokenConverter() {
+		return new JwtAccessTokenConverter();
+	}
 	
 }
