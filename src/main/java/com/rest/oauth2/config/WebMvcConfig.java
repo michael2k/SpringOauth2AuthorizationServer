7e66ed3e55a7d1565cbfaaefab9a707a4effe27c
michael2k <kms@noaats.com>
2020-03-01 12:41:20 +0900

    2차 테스

--- src/main/java/com/rest/oauth2/config/WebMvcConfig.java
+++ src/main/java/com/rest/oauth2/config/WebMvcConfig.java
@@ -1,6 +1,8 @@
 package com.rest.oauth2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,5 +21,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .maxAge(MAX_AGE_SECONDS);
     }
 
+    @Bean
+    public RestTemplate getRestTemplate() {
+    	return new RestTemplate();
+    }
 
 }
